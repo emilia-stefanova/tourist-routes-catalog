32695c53bd8356a7c0d4ac941a528b1a4b06c797
Stanislav Atanasov <mastxp@gmail.com>
2014-04-27 18:10:18 +0300

    added routeimage proxy and model.

---
+++ TouristRouteCatalog.Core/Model/RouteImageModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouristRouteCatalog.Core.Proxy.RouteImage;
+using TouristRouteCatalog.Core.Repository;
+
+namespace TouristRouteCatalog.Core.Model
+{
+    public class RouteImageModel : IModel
+    {
+        #region Repositories
+        [Dependency]
+        public RouteImageRepo routeImageRepo { get; set; }
+        #endregion
+
+        public void Init()
+        {
+
+        }
+
+        public List<RouteImageProxy> GetAllImagesForRoute(int routeId)
+        {
+            return routeImageRepo.GetAllImagesForRoute(routeId);
+        }
+
+        public RouteImageProxy GetImageRouteForId(int imageId)
+        {
+            return routeImageRepo.GetImageRouteForId(imageId);
+        }
+
+        public void SaveImages(List<RouteImageProxy> routes)
+        {
+            routes.ForEach(item =>
+            {
+                routeImageRepo.Add(item);
+            });
+            routeImageRepo.SaveChanges();
+        }
+    }
+}
